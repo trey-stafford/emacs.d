b432c8b21191a2619c4509ed6b5a015f1bb6026d
Trey Stafford <trey.stafford@nsidc.org>
2017-04-24 16:34:56 -0600

    Add custom keybindings for magit

--- init.el
+++ init.el
@@ -119,7 +119,16 @@
 
 (use-package evil-magit
   :ensure t)
-(general-define-key "C-X g" 'magit-status)
+(general-define-key :prefix ","
+		    "g" 'magit-status
+		    "l" 'magit-log-buffer-file
+		    "c" 'magit-show-refs
+		    "L" 'magit-log-current
+		    "b" 'magit-blame
+		    "p" 'magit-push-popup
+		    "u" 'magit-pull-from-upstream
+		    "d" 'magit-diff-buffer-file-popup)
+		    
 
 ; Evil-mode remaps
 (general-define-key "C-k" 'evil-window-up
