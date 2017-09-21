ae032accc726bf774d9c6af75846a4290f46c44d
Trey Stafford <trey.stafford@nsidc.org>
2017-09-21 10:15:02 -0600

    Current config sep 21, 2017
    WIP commit. Working on cleaning this up.

--- init.el
+++ init.el
@@ -21,6 +21,10 @@
 (eval-when-compile
   (require 'use-package))
 
+(use-package exec-path-from-shell
+  :ensure t)
+(exec-path-from-shell-initialize)
+
 ; Now we can ensure the markdown-mode
 ; package is automatically installed
 (use-package markdown-mode
@@ -45,10 +49,10 @@
 ;(customize-set-variable relative-line-numbers-format relative-line-numbers-formatter)
 
 ; Theme
-(use-package darkburn-theme
+(use-package monokai-theme
   :ensure t
   :config
-  (load-theme 'darkburn t))
+  (load-theme 'monokai t))
 
 ; Evil mode.
 (use-package evil
@@ -124,6 +128,7 @@
 ; Magit
 (use-package magit
   :ensure t)
+(setq magit-diff-refine-hunk 'all)
 
 (use-package evil-magit
   :ensure t)
@@ -266,9 +271,6 @@
     "l" 'org-open-at-point
     "t" 'org-todo)
 
-; Turn on pretty symbols in org mode by default.
-(customize-set-variable 'org-pretty-entities t)
-
 ; Set the list of org TODO states.
 ; (<letetr>) == shortcut key
 ; (<letter>!) == log time
@@ -290,3 +292,145 @@
 ; The following line would disable symbolic link lock files. May be necessary to prevent rsync problems when provisioning machines.
 ; (setq create-lockfiles nil)
 
+
+; jsx-mode
+; see https://github.com/jsx/jsx-mode.el/blob/develop/init.el.example
+;(use-package jsx-mode
+;  :ensure t
+;  :config
+;  (add-to-list 'auto-mode-alist '("\\.jsx\\'" . jsx-mode)))
+;
+;(autoload 'jsx-mode "jsx-mode" "JSX mode" t)
+;(setq jsx-indent-level 2)
+
+(add-hook 'web-mode-hook
+      (lambda ()
+        (setq tab-width 2)))
+
+(add-hook 'jsx-mode-hook
+      (lambda ()
+        (setq tab-width 2)))
+
+; Otherwise disabled
+(put 'narrow-to-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+
+
+; make tabs 2 spaces in js
+(setq js-indent-level 2)
+
+; Use visual line mode everywhere.
+(setq visual-line-mode t)
+
+
+; zoom window
+(use-package zoom-window
+    :ensure t
+    :config
+    (setq zoom-window-mode-line-color "DarkGreen"))
+
+; Toggle zoom with SPC-z
+(general-define-key :prefix "SPC"
+		    "z" 'zoom-window-zoom)
+
+; imenu with SPC-m
+(general-define-key :prefix "SPC"
+		    "m" 'helm-imenu)
+
+; CSS mode 
+(use-package css-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.less\\'" . css-mode)))
+
+(add-hook 'css-mode-hook
+      (lambda ()
+        (setq tab-width 2)))
+
+; Set default column fill to 80 chars
+(setq-default fill-column 80)
+
+; Ein
+(use-package ein
+    :ensure t)
+
+; Make dired report space in human readable way
+(setq dired-listing-switches "-alh")
+
+; Set global font size
+(set-face-attribute 'default nil :height 90)
+
+; TODO: http://codewinds.com/blog/2015-04-02-emacs-flycheck-eslint-jsx.html#!
+; Flycheck
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+; javascript stuff
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+
+;; disable json-jsonlist checking for json files
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(json-jsonlist)))
+
+;; disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(javascript-jshint)))
+
+;; adjust indents for web-mode to 2 spaces
+(defun my-web-mode-hook ()
+  "Hooks for Web mode. Adjust indents"
+  ;;; http://web-mode.org/
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+;; for better jsx syntax-highlighting in web-mode
+;; - courtesy of Patrick @halbtuerke
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+    (let ((web-mode-enable-part-face nil))
+      ad-do-it)
+    ad-do-it))
+
+;; use local eslint from node_modules before global
+;; http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
+
+(use-package puppet-mode
+  :ensure t
+  :init (global-flycheck-mode))
+
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
+; Ace window makes switching windows veasy
+(use-package ace-window
+  :ensure t)
+
+; Make ace-window selectors be home-row keys
+(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+
+; space-a activates ace-window
+(general-define-key :prefix "SPC"
+		    "a" 'ace-window)
+
+
+; helm popup at bottom of frame.
+(add-to-list 'display-buffer-alist
+                    `(,(rx bos "*helm" (* not-newline) "*" eos)
+                         (display-buffer-in-side-window)
+                         (inhibit-same-window . t)
+                         (window-height . 0.4)))
