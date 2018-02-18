28ff0ff17cc08d473f3950fce1a0368fecf362ba
Trey Stafford <trey.stafford@nsidc.org>
2018-02-18 12:31:02 -0700

    Make evil-mode up/down operate in screen lines instead of logical

--- init.el
+++ init.el
@@ -50,6 +50,13 @@
   :config
   (evil-mode t))
 
+;; Make evil-mode up/down operate in screen lines instead of logical lines
+(define-key evil-motion-state-map "j" 'evil-next-visual-line)
+(define-key evil-motion-state-map "k" 'evil-previous-visual-line)
+;; Also in visual mode
+(define-key evil-visual-state-map "j" 'evil-next-visual-line)
+(define-key evil-visual-state-map "k" 'evil-previous-visual-line)
+
 ; Helm
 (use-package helm
   :ensure t
