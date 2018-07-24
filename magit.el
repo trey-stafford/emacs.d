a4522c60874340dd5c2429ff77efdd3d89eda1e2
Trey Stafford <trey.stafford@nsidc.org>
2018-07-24 14:45:33 -0600

    test custom keys for moving rebase lines

--- magit.el
+++ magit.el
@@ -14,3 +14,8 @@
 		    "p" 'magit-push-popup
 		    "u" 'magit-pull-from-upstream
 		    "d" 'magit-diff-buffer-file-popup)
+
+; use CTL-K/J to move rebase lines up and down in magit.
+(with-eval-after-load 'git-rebase
+  (evil-magit-define-key evil-magit-state 'git-rebase-mode-map "C-K" 'git-rebase-move-line-up)
+  (evil-magit-define-key evil-magit-state 'git-rebase-mode-map "C-J" 'git-rebase-move-line-down))
