2e648ba922c205fe6aea1a7ba594d5e34afd0480
Trey Stafford <trey.stafford@nsidc.org>
2018-07-25 12:43:43 -0600

    Add git-timemachine

--- magit.el
+++ magit.el
@@ -11,6 +11,19 @@
   (evil-magit-define-key evil-magit-state 'git-rebase-mode-map "C-K" 'git-rebase-move-line-up)
   (evil-magit-define-key evil-magit-state 'git-rebase-mode-map "C-J" 'git-rebase-move-line-down))
 
+; git-timemachine
+(use-package git-timemachine
+  :ensure t)
+
+; override evil-mode bindings
+; see https://github.com/emacs-evil/evil/issues/511
+(eval-after-load 'git-timemachine
+  '(progn
+     (evil-make-overriding-map git-timemachine-mode-map 'normal)
+     ;; force update evil keymaps after git-timemachine-mode loaded
+     (add-hook 'git-timemachine-mode-hook #'evil-normalize-keymaps)))
+
+; git related keybindings.
 (general-define-key :prefix ","
 		    "g" 'magit-status
 		    "l" 'magit-log-buffer-file
@@ -19,9 +32,5 @@
 		    "b" 'magit-blame
 		    "p" 'magit-push-popup
 		    "u" 'magit-pull-from-upstream
-		    "d" 'magit-diff-buffer-file-popup)
-
-; use CTL-K/J to move rebase lines up and down in magit.
-(with-eval-after-load 'git-rebase
-  (evil-magit-define-key evil-magit-state 'git-rebase-mode-map "C-K" 'git-rebase-move-line-up)
-  (evil-magit-define-key evil-magit-state 'git-rebase-mode-map "C-J" 'git-rebase-move-line-down))
+		    "d" 'magit-diff-buffer-file-popup
+            "t" 'git-timemachine-toggle)
