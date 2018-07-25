c00427a02944ab433c664400fc898e2c839a73a6
Trey Stafford <trey.stafford@nsidc.org>
2018-07-25 12:43:12 -0600

    Keybindings for moving git rebase lines up and down

--- magit.el
+++ magit.el
@@ -5,6 +5,12 @@
 
 (use-package evil-magit
   :ensure t)
+
+; use CTL-K/J to move rebase lines up and down in magit.
+(with-eval-after-load 'git-rebase
+  (evil-magit-define-key evil-magit-state 'git-rebase-mode-map "C-K" 'git-rebase-move-line-up)
+  (evil-magit-define-key evil-magit-state 'git-rebase-mode-map "C-J" 'git-rebase-move-line-down))
+
 (general-define-key :prefix ","
 		    "g" 'magit-status
 		    "l" 'magit-log-buffer-file
