b938d9aeb5b16be8314a2969da3511365924939f
Trey Stafford <trey.stafford@nsidc.org>
2018-07-25 12:40:49 -0600

    Update custom.el

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
     ("f78de13274781fbb6b01afd43327a4535438ebaeec91d93ebdbba1e3fba34d3c" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(package-selected-packages
    (quote
-    (emacs-anywhere slime persp-projectile eyebrowse perspective indent-guide rvm sx ruby-electric popwin swiper ace-window evil-mc puppet-mode flymake-puppet exec-path-from-shell flycheck monokai-theme ein zoom-window sr-speedbar json-mode web-mode jsx-mode rainbow-mode gnuplot-mode gnuplot auto-indent-mode org-evil auto-highlight-symbol highlight-symbol evil-magit hackernews camcorder realgud elpy yaml-mode helm-projectile projectile solarized-theme relative-line-numbers general helm magit markdown-mode use-package evil-visual-mark-mode))))
+    (git-timemachine yasnippet-snippets evil-surround helm-tramp emacs-anywhere slime persp-projectile eyebrowse perspective indent-guide rvm sx ruby-electric popwin swiper ace-window evil-mc puppet-mode flymake-puppet exec-path-from-shell flycheck monokai-theme ein zoom-window sr-speedbar json-mode web-mode jsx-mode rainbow-mode gnuplot-mode gnuplot auto-indent-mode org-evil auto-highlight-symbol highlight-symbol evil-magit hackernews camcorder realgud elpy yaml-mode helm-projectile projectile solarized-theme relative-line-numbers general helm magit markdown-mode use-package evil-visual-mark-mode))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
