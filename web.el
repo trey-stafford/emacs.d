37b5765e4e62ac28cd563ba2ebd2d5a760745730
Trey Stafford <trey.stafford@colorado.edu>
2026-04-24 17:40:31 -0600

    Remove OBE typescript ts lint checker

--- web.el
+++ web.el
@@ -87,8 +87,6 @@
           (lambda ()
             (when (string-equal "tsx" (file-name-extension buffer-file-name))
               (setup-tide-mode))))
-;; enable typescript-tslint checker
-(flycheck-add-mode 'typescript-tslint 'web-mode)
 
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 (add-hook 'web-mode-hook
